34915208c0035edd8615603c5ac37f9f5b7f39ae
Alexander Kolomiets <kaa5843771@yandex.ru>
2021-12-22 23:58:39 +0300

    init

---
+++ src/app/main/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function MainService() {
+}
+
+function MainCtrl($scope, MainService) {
+  $scope.main = "main";
+}
+
+angular
+  .module('pocketInvestor.main', ['ngRoute'])
+  .service('MainService', MainService)
+  .controller('MainCtrl', ['$scope', 'MainService', MainCtrl])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/main', {
+      templateUrl: 'app/main/main.html',
+      controller: 'MainCtrl'
+    });
+  }]);
+
